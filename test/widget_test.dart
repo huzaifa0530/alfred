d9cb012c778a89dcf2e3b7228a67751ecb1d0d0d
huzaifa0530 <mhuzaifa0530@gmail.com>
2026-09-07 22:21:48 +0500

    check

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,13 +11,14 @@ import 'package:flutter_test/flutter_test.dart';
 
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter in smoke test aaaaaaaaaaaaaaaaaaaa', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const AlfredApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
+    expect(find.text('2'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
